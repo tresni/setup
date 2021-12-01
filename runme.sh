1d4fe625e51a4d256a20c78a6224a2c360a59507
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2021-11-30 21:18:43 -0700

    Updates after using on the new mac

--- runme.sh
+++ runme.sh
@@ -1,7 +1,29 @@
 #! /usr/bin/env bash
 
+function validate_keys {
+	LOADED=$(ssh-add -ql)
+	if ! [ "$?" -eq "0" ]; then
+		return 1
+	fi
+	KEYS=$(ssh-keygen -lf <(curl -s https://github.com/tresni.keys) 2> /dev/null | awk '{ print $2 }')
+	if [ -z "$KEYS" ]; then
+		return 1
+	fi
+	for key in "${KEYS[@]}"; do
+		if grep -q "$key" <(echo "$LOADED"); then
+			return 0
+		fi
+	done
+	return 1
+}
+
 sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+if ! validate_keys; then
+	echo "Load your SSH keys first"
+	exit 1
+fi
 
 if hash brew 2>/dev/null; then
 	echo "Homebrew is already installed!"
@@ -15,8 +37,15 @@ else
 	fi
 fi
 
-brew bundle install --file=Brewfile --no-lock
-brew cleanup
+BREW_PATH=/usr/local/bin/brew
+
+if ! [ -e "$BREW_PATH" ]; then
+	BREW_PATH=/opt/homebrew/bin/brew
+fi
+
+
+"$BREW_PATH" bundle install --file=Brewfile --no-lock
+"$BREW_PATH" cleanup
 
 if [ -e ~/.oh-my-zsh ]; then
     echo "Oh My ZSH already installed"
@@ -33,6 +62,10 @@ fi
 if [ -e ~/.dotfiles ]; then
     echo "dotfiles are already installed"
 else
+    if [ -e ~/.zshrc]; then
+		echo "backing up ~/.zshrc to ~/.zshrc_setup"
+		mv ~/.zshrc ~/.zshrc_setup
+	fi
     curl -L http://bit.ly/tresni-dotfiles | bash
     exit_status=$?
 	if [ $exit_status != 0 ]; then
